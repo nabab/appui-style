e49d5674533c1a8569f855959b1b5b187ec6d57c
Thomas Nabet <thomas.nabet@gmail.com>
2021-06-29 12:39:08 +0200

    Manu!

---
+++ src/mvc/html/manu.php
@@ -0,0 +1,586 @@
+<!-- HTML Document -->
+
+<div class="bbn-overlay manu">
+  <h1>
+    CSS Library demo
+  </h1><br>
+   <h2>Backgrounds</h2><br>
+   <div class="bbn-bordered bbn-background bbn-15">
+    <div class="bbn-bg-olive">
+      bbn-background
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-background-effect bbn-15">
+    <div class="bbn-bg-olive">
+      bbn-background-effect
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-alt-background bbn-15">
+    <div class="bbn-bg-olive">
+      bbn-alt-background
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-alt-background-effect bbn-15">
+    <div class="bbn-bg-olive">
+      bbn-alt-background-effect
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-background-effect-primary bbn-15">
+    <div class="bbn-bg-olive">
+      bbn-background-effect-primary
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-background-effect-secondary bbn-15">
+    <div class="bbn-bg-olive">
+      bbn-background-effect-secondary
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-background-effect-tertiary bbn-15">
+    <div class="bbn-bg-olive">
+      bbn-background-effect-tertiary
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-background-effect-hover bbn-15">
+    <div class="bbn-bg-olive">
+      bbn-background-effect-hover
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-background-header bbn-15">
+    <div class="bbn-bg-olive">
+      bbn-background-header
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-background-header-alt bbn-15">
+    <div class="bbn-bg-olive">
+      bbn-background-header-alt
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-hover-background bbn-15">
+    <div class="bbn-bg-olive">
+      bbn-hover-background
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-active-background bbn-15">
+    <div class="bbn-bg-olive">
+      bbn-active-background
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-alt-dark bbn-15">
+    <div class="bbn-bg-olive">
+      bbn-alt-dark
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-alt-light bbn-15">
+    <div class="bbn-bg-olive">
+      bbn-alt-light
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-modal bbn-15">
+    <div class="bbn-bg-olive">
+      bbn-modal
+    </div>
+  </div>
+  <hr>
+   <h2>Colors & Background Colors</h2><br>
+   <div class="bbn-bordered bbn-bg-black bbn-15">
+    <div class="bbn-black bbn-bg-beige">
+      bbn-black || bbn-bg-black
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-bg-anthracite bbn-15">
+    <div class="bbn-anthracite bbn-bg-beige">
+      bbn-anthracite || bbn-bg-anthracite
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-orange bbn-15">
+    <div class="bbn-orange bbn-bg-black">
+      bbn-orange || bbn-bg-orange
+    </div>
+  </div>
+    <div class="bbn-bordered bbn-bg-red bbn-15">
+    <div class="bbn-red bbn-bg-black">
+      bbn-red || bbn-bg-red
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-green bbn-15">
+    <div class="bbn-green bbn-bg-black">
+      bbn-green || bbn-bg-green
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-blue bbn-15">
+    <div class="bbn-blue bbn-bg-black">
+      bbn-blue || bbn-bg-blue
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-yellow bbn-15">
+    <div class="bbn-yellow bbn-bg-black">
+      bbn-yellow || bbn-bg-yellow
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-beige bbn-15">
+    <div class="bbn-beige bbn-bg-black">
+      bbn-beige || bbn-bg-beige
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-white bbn-15">
+    <div class="bbn-white bbn-bg-black">
+      bbn-white || bbn-bg-white
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-purple bbn-15">
+    <div class="bbn-purple bbn-bg-black">
+      bbn-purple || bbn-bg-purple
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-grey bbn-15">
+    <div class="bbn-grey bbn-bg-black">
+      bbn-grey || bbn-bg-grey
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-brown bbn-15">
+    <div class="bbn-brown bbn-bg-black">
+      bbn-brown || bbn-bg-brown
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-olive bbn-15">
+    <div class="bbn-olive bbn-bg-black">
+      bbn-olive || bbn-bg-olive
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-pink bbn-15">
+    <div class="bbn-pink bbn-bg-black">
+      bbn-pink || bbn-bg-pink
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-turquoise bbn-15">
+    <div class="bbn-turquoise bbn-bg-black">
+      bbn-turquoise || bbn-bg-turquoise
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-cyan bbn-15">
+    <div class="bbn-cyan bbn-bg-black">
+      bbn-cyan || bbn-bg-cyan
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-navy bbn-15">
+    <div class="bbn-navy bbn-bg-beige">
+      bbn-navy || bbn-bg-navy
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-darkgrey bbn-15">
+    <div class="bbn-darkgrey bbn-bg-beige">
+      bbn-darkgrey || bbn-bg-darkgrey
+    </div>
+  </div>
+  <hr>
+   <h2>Containers Width</h2><br>
+   <div class="bbn-bordered bbn-bg-turquoise bbn-wider bbn-10">
+    <div class="bbn-bg-olive">
+      bbn-wider
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-bg-turquoise bbn-wide bbn-10">
+    <div class="bbn-bg-olive">
+      bbn-wide
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-bg-turquoise bbn-regular bbn-10">
+    <div class="bbn-bg-olive">
+      bbn-regular
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-bg-turquoise bbn-narrow bbn-10">
+    <div class="bbn-bg-olive">
+      bbn-narrow
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-bg-turquoise bbn-narrower bbn-10">
+    <div class="bbn-bg-olive">
+      bbn-narrower
+    </div>
+  </div>
+  <hr>
+  
+   <h2>Margins</h2><br>
+   <div class="bbn-bordered bbn-bg-turquoise bbn-hmargin bbn-10">
+    <div class="bbn-bg-olive">
+      bbn-hmargin
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-turquoise bbn-vmargin bbn-10">
+    <div class="bbn-bg-olive">
+      bbn-vmargin
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-turquoise bbn-margin bbn-10">
+    <div class="bbn-bg-olive">
+      bbn-margin
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-hlmargin bbn-bg-turquoise bbn-10">
+    <div class="bbn-bg-olive">
+      bbn-hlmargin
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-turquoise bbn-vlmargin bbn-10">
+    <div class="bbn-bg-olive">
+      bbn-vlmargin
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-turquoise bbn-lmargin bbn-10">
+    <div class="bbn-bg-olive">
+      bbn-lmargin
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-turquoise bbn-hsmargin bbn-10">
+    <div class="bbn-bg-olive">
+      bbn-hsmargin
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-bg-turquoise bbn-vsmargin bbn-10">
+    <div class="bbn-bg-olive">
+      bbn-vsmargin
+    </div>
+  </div>
+  <hr>
+  
+  <h2>Paddings</h2>
+   <div class="bbn-bordered bbn-no-padding bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-no-hpadding bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-no-vpadding bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-hlpadded bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-vlpadded bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-lpadded bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-hpadded bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-vpadded bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-padded bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-hspadded bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-vspadded bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-spadded bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-xhspadded bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-vxspadded bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div> <div class="bbn-bordered bbn-xspadded bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-hxxspadded bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-vxxspadded bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-xxspadded bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <hr>
+  
+  <h2>Spacing</h2>
+     <div class="bbn-bordered bbn-top-space bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-top-lspace bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-top-sspace bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-top-xsspace bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-bottom-space bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-bottom-lspace bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-bottom-sspace bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-bottom-xsspace bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-left-space bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-left-lspace bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-left-sspace bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-left-xsspace bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <div class="bbn-bordered bbn-right-space bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-right-lspace bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-right-sspace bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-right-xsspace bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+  <hr>
+  
+   <h2>States</h2>
+   <div class="bbn-bordered bbn-state-default bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-state-hover bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-state-active bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-state-selected bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-state-error bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div> <div class="bbn-bordered bbn-error-colored bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-state-empty bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-state-focus bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-reactive bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-primary bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-secondary bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-tertiary bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+   <div class="bbn-bordered bbn-disabled bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+  </div>
+     <div class="bbn-bordered bbn-negative bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+     </div>
+     <div class="bbn-bordered bbn-unselectable bbn-10">
+    <div class="bbn-bg-olive">
+      Hello
+    </div>
+     </div>
+  <hr>
+  
+    <h2>Widths</h2>
+   <div class="bbn-no-padding bbn-clear bbn-w-5">
+    <div class="bbn-bordered bbn-border-black bbn-bg-blue">
+      bbn-w-5
+    </div>
+  </div><br>
+   <div class="bbn-no-padding bbn-clear bbn-w-10">
+    <div class="bbn-bg-olive">
+      bbn-w-10
+    </div>
+  </div><br>
+   <div class="bbn-no-padding bbn-clear bbn-w-15">
+    <div class="bbn-bordered bbn-border-black bbn-bg-orange">
+      bbn-w-15
+    </div>
+  </div><br>
+   <div class="bbn-no-padding bbn-clear bbn-w-20">
+    <div class="bbn-bg-olive">
+      bbn-w-20
+    </div>
+  </div><br>
+   <div class="bbn-no-padding bbn-clear bbn-w-25">
+    <div class="bbn-bordered bbn-border-black bbn-bg-red">
+      bbn-w-25
+    </div>
+  </div><br>
+   <div class="bbn-no-padding bbn-clear bbn-w-30">
+    <div class="bbn-bg-olive">
+      bbn-w-30
+    </div>
+  </div><br>
+   <div class="bbn-no-padding bbn-clear bbn-w-33">
+    <div class="bbn-bordered bbn-border-black bbn-bg-yellow">
+      bbn-w-33
+    </div>
+  </div><br>
+   <div class="bbn-no-padding bbn-clear bbn-w-40">
+    <div class="bbn-bg-olive">
+      bbn-w-40
+    </div>
+  </div><br>
+   <div class="bbn-no-padding bbn-clear bbn-w-50">
+    <div class="bbn-bordered bbn-border-black bbn-bg-brown">
+      bbn-w-50
+    </div>
+  </div><br>
+   <div class="bbn-no-padding bbn-clear bbn-w-60">
+    <div class="bbn-bg-olive">
+      bbn-w-60
+    </div>
+  </div><br>
+   <div class="bbn-no-padding bbn-clear bbn-w-66">
+    <div class="bbn-bordered bbn-border-black bbn-bg-purple">
+      bbn-w-66
+    </div>
+  </div><br>
+   <div class="bbn-no-padding bbn-clear bbn-w-70">
+    <div class="bbn-bg-olive">
+      bbn-w-70
+    </div>
+  </div><br>
+   <div class="bbn-no-padding bbn-clear bbn-w-75">
+    <div class="bbn-bordered bbn-border-black bbn-bg-pink">
+      bbn-w-75
+    </div>
+  </div><br>
+   <div class="bbn-no-padding bbn-clear bbn-w-80">
+    <div class="bbn-bg-olive">
+      bbn-w-80
+    </div>
+  </div><br>
+   <div class="bbn-no-padding bbn-clear bbn-w-90">
+    <div class="bbn-bordered bbn-border-black bbn-bg-grey">
+      bbn-w-90
+    </div>
+  </div><br>
+   <div class="bbn-no-padding bbn-full-width || bbn-clear bbn-w-100">
+    <div class="bbn-bg-olive">
+      bbn-full-width ||bbn-w-100
+    </div>
+  </div><br>
+   <div class="bbn-no-padding bbn-flex-width bbn-flex-fill">
+    <div class="bbn-bordered bbn-border-black bbn-bg-cyan">
+      div parent class bbn-flex-width
+    </div>
+  </div><br>
+  <hr>
+</div>

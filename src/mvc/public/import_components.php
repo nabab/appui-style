849d73b4c69318a7c507c98cb07cbb92a8373481
Nabab <thomas.nabet@gmail.com>
2021-12-08 22:50:12 +0100

    Removed some useless comments

--- src/mvc/public/import_components.php
+++ src/mvc/public/import_components.php
@@ -1,8 +1,4 @@
 <?php
-/*
- * Describe what it does to show you're not that dumb!
- *
- **/
 
 /** @var $this \bbn\Mvc\Controller */
 $ctrl->combo("Components importation", $ctrl->getModel('./appui_components'));

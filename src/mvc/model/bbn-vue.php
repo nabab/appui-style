ca1f56f607cae9c95b35f391ff4f9d47bc325ecb
nabab <thomas.nabet@gmail.com>
2024-11-11 00:23:25 +0100

    Fix in comments for controller and model (wrong order)

--- src/mvc/model/bbn-vue.php
+++ src/mvc/model/bbn-vue.php
@@ -4,7 +4,7 @@
  *
  **/
 
-/** @var $model \bbn\Mvc\Model*/
+/** @var bbn\Mvc\Model $model */
 if ( isset($model->data['structure']) ){
   $file = BBN_DATA_PATH.'bbn-vue.json';
   $data = $model->data['structure'];
